210539b86efaa911a4e1772e11f59cac4f65ae9a
Ethan <67290322+ethanshamilton@users.noreply.github.com>
2026-09-22 18:50:17 -0400

    Solve matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def matrix_transpose(A: list) -> np.ndarray:
+    """
+    Returns the transposed matrix as a NumPy array.
+    """
+    # create rows
+    transposed = [[] for i in range(len(A[0]))]
+
+    print(transposed)
+    
+    # matrix transpose
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            transposed[j].append(A[i][j])
+
+    return np.asarray(transposed)
